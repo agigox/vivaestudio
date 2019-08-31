38bcc65728e33a025b4126a24ced3331214b5f00
agigox <amin.tabou@gmail.com>
2019-08-31 18:50:26 +0200

    Adding react-helmet package for SEO concerns

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,9 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Helmet } from "react-helmet";
 
 export default class About extends Component {
     render() {
         return (
             <div>
+                <Helmet>
+                    <title>My seo app</title>
+                    <meta name="description" content="testing react helmet" />
+                    <meta name="keywords" content="react,seo,helmet" />
+                </Helmet>
                 This is the about page
             </div>
         )
