fccd5b1a83f62ad904da855cab4a39b2367b151c
Amine TABOU <amin.tabou@gmail.com>
2019-09-05 15:52:03 +0200

    Migrating the content and styles from create-react-app to gatsby

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,26 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+/*
+import { Link } from "gatsby";
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from "../components/layout";
+import Image from "../components/image";
+import SEO from "../components/seo";
+*/
 
+const IndexPage = () => (
+  <div>
+    <h2>Diseño web y desarrollo front-end</h2>
+      
+    <p>Más de 12 años de experiencia diseñando interfaces y desarrollando código front-end. Uso Sketch, WordPress, HTML5, SCSS, jQuery y Gulp, entre otros.</p>
+    
+    <p>Ahora, al frente de un pequeño equipo de diseñadores/desarrolladores front-end en <a href="https://www.techpump.com" target="_blank" rel="noopener">Techpump</a> en Gijón, Asturias.</p>
+    
+    <p>También me puedes encontrar en <a href="https://dribbble.com/alv" target="_blank" rel="noopener">dribbble</a>, <a href="https://twitter.com/alv" target="_blank" rel="noopener">Twitter</a>, <a href="https://instagram.com/alvcm" target="_blank" rel="noopener">Instagram</a>, <a href="https://linkedin.com/alvarocastano" target="_blank" rel="noopener">LinkedIn</a> o <a href="https://facebook.com/alvarocastano" target="_blank" rel="noopener">Facebook</a>.</p>
+    
+    <p>Puedes escribirme a alvaro (at) vivaestudio.com</p>
+  </div>
+)
+/*
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
@@ -18,5 +34,6 @@ const IndexPage = () => (
     <Link to="/page-3/">Go to page 3</Link>
   </Layout>
 )
+*/
 
 export default IndexPage
